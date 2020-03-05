07c577fbc0d45fa7507c994d24b0bef1d9a1d36a
Elbert Bautista <ebautista@broadleafcommerce.org>
2020-03-05 16:44:17 -0600

    add cors filter to support open api swagger service

--- src/main/java/com/broadleafsamples/tutorials/TutorialGettingStartedApplication.java
+++ src/main/java/com/broadleafsamples/tutorials/TutorialGettingStartedApplication.java
@@ -2,9 +2,15 @@ package com.broadleafsamples.tutorials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.broadleafcommerce.resource.security.SecurityEnhancer;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class TutorialGettingStartedApplication {
@@ -25,6 +31,20 @@ public class TutorialGettingStartedApplication {
             http.authorizeRequests().antMatchers("/resolver/**").permitAll();
             http.authorizeRequests().antMatchers("/url-resolver/**").permitAll();
         };
+    }
+
+    @Bean
+    @ConditionalOnProperty("tutorial.cors.filter.enabled")
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list of allowed origins
+        config.setAllowedOrigins(Collections.singletonList(CorsConfiguration.ALL));
+        config.setAllowedHeaders(Collections.singletonList(CorsConfiguration.ALL));
+        config.setAllowedMethods(Collections.singletonList(CorsConfiguration.ALL));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     public static void main(String[] args) {
