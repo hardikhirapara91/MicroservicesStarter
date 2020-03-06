abd0063a7f387d3992622c8860edacd806b29b84
Samarth Dhruva <sdhruva@broadleafcommerce.com>
2020-03-06 16:28:23 -0600

    Add custom ContextStateBuilder for no-security configuration that allows for mutable ContextState

--- src/main/java/com/broadleafsamples/tutorials/TutorialGettingStartedApplication.java
+++ src/main/java/com/broadleafsamples/tutorials/TutorialGettingStartedApplication.java
@@ -1,16 +1,25 @@
 package com.broadleafsamples.tutorials;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import com.broadleafcommerce.common.extension.TypeFactory;
+import com.broadleafcommerce.data.tracking.core.Trackable;
+import com.broadleafcommerce.data.tracking.core.TrackableBehaviorUtil;
+import com.broadleafcommerce.data.tracking.core.context.ContextInfo;
+import com.broadleafcommerce.data.tracking.core.mapping.ContextStateBuilder;
 import com.broadleafcommerce.resource.security.SecurityEnhancer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Collections;
+import lombok.extern.apachecommons.CommonsLog;
 
 @SpringBootApplication
 public class TutorialGettingStartedApplication {
@@ -45,6 +54,26 @@ public class TutorialGettingStartedApplication {
         config.setAllowedMethods(Collections.singletonList(CorsConfiguration.ALL));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+    @Component
+    @ConditionalOnProperty(name = "broadleaf.common.policy.validation.enforce",
+                            havingValue = "false")
+    @CommonsLog
+    static class AlwaysMutableContextStateBuilder extends ContextStateBuilder {
+
+        public AlwaysMutableContextStateBuilder(TypeFactory factory,
+                                                TrackableBehaviorUtil behaviorUtil, ObjectMapper fieldChangeMapper,
+                                                ModelMapper cloneMapper) {
+            super(factory, behaviorUtil, fieldChangeMapper, cloneMapper);
+            log.info("Loaded AlwaysMutableContextStateBuilder");
+        }
+
+        @Override
+        protected boolean determineContextStateMutability(Trackable domain,
+                                                          ContextInfo contextInfo) {
+            return true;
+        }
     }
 
     public static void main(String[] args) {
